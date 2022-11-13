2c13e35bb3fa2668b540d8d0b9cc53d0d57a3eae
costikluciu2020 <costikluciu2020@gmail.com>
2022-11-13 11:39:33 +0000

    Connected my page to Quotable API, a free and open source api made by lukePeavey

--- script/script.js
+++ script/script.js
@@ -11,15 +11,38 @@ $("#new-quote").click(()=>{
 	let greenNumber = 0 + Math.floor(Math.random() * 255);	
 	let blueNumber = 0 + Math.floor(Math.random() * 255);	
 
-	$("body").get(0).style.setProperty("--bg-color",`rgb(${redNumber}, ${greenNumber} , ${blueNumber})`  );	
+	$("body").get(0).style.setProperty("--bg-color",`rgb(${redNumber}, ${greenNumber} , ${blueNumber})`  );
+
+
+	
+	// Fetch some data from an API.
+	// I will use Quotable, a free and open source api, repository address: 
+	// https://github.com/lukePeavey/quotable#get-random-quote
+	// Special thanks to: lukePeavey for making this possible.
+
+	$.ajax({
+		type: "GET", 
+		url: "https://api.quotable.io/random",
+		success: function(data){
+		console.log("data=", data);
+
+		$("#text").text(data.content);
+		$("#author").text(data.author);
+
+		}
+	})
+
+
+
+
+
+
+
+
 });
 
 
 //$("blockquote").css("backgroundColor", "yellow");
-
-
-
-
 
 
 
