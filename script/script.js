663ed366ceadc534de0157700637c6777179d28b
costikluciu2020 <costikluciu2020@gmail.com>
2022-11-13 10:45:46 +0000

    Implemented the random color feature

--- script/script.js
+++ script/script.js
@@ -3,11 +3,19 @@ console.log("test test are you there");
 
 
 $("#new-quote").click(()=>{
-	console.log("foo click");
+	//console.log("color is:", rgb(200, 255, 10) );
+
+	//$("body").get(0).style.setProperty("$("body").get(0).style.setProperty("--bg-color", "hotpink");", "black");
+	//$("body").get(0).style.setProperty("--bg-color", "hotpink");	
+	let redNumber = 0 + Math.floor(Math.random() * 255);		
+	let greenNumber = 0 + Math.floor(Math.random() * 255);	
+	let blueNumber = 0 + Math.floor(Math.random() * 255);	
+
+	$("body").get(0).style.setProperty("--bg-color",`rgb(${redNumber}, ${greenNumber} , ${blueNumber})`  );	
 });
 
 
-
+//$("blockquote").css("backgroundColor", "yellow");
 
 
 
