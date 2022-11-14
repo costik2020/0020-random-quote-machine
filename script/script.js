3fd300cfac0839068abc70a7cc33dcd441dad238
costikluciu2020 <costikluciu2020@gmail.com>
2022-11-14 11:13:10 +0000

    Added the buttons functionality and some quotes and fixed the color for the author line '--'

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 console.log("test test are you there");
 
-
+// If the user did not press `New quote` button
+let newQuotesRequested = 0;
 
 $("#new-quote").click(()=>{
 	//console.log("color is:", rgb(200, 255, 10) );
@@ -24,11 +25,21 @@ $("#new-quote").click(()=>{
 		type: "GET", 
 		url: "https://api.quotable.io/random",
 		success: function(data){
-		console.log("data=", data);
+			console.log("data=", data);
 
-		$("#text").text(data.content);
-		$("#author").text(data.author);
+			$("#text").text(data.content);
+			$("#author").text(data.author);
 
+
+			// Enable the twitter button
+			let newLink="";
+			newLink = `https://twitter.com/intent/tweet?text=${data.content} by ${data.author}`;
+			//newLink = newLink.replace(" ", "zzzzzzzz");
+			newLink = newLink.replace(/\s/g, "+");
+			$("#tweet-quote").attr("href", newLink);
+			console.log("newLink for twitter=", newLink);
+
+			newQuotesRequested = newQuotesRequested + 1;
 		}
 	})
 
@@ -38,8 +49,23 @@ $("#new-quote").click(()=>{
 
 
 
-
 });
+
+
+if (newQuotesRequested == 0 ){
+
+	let newLink="";
+	newLink = `https://twitter.com/intent/tweet?text= It does not matter how slowly you go as long as you do not stop. by Confucius `;
+	//newLink = newLink.replace(" ", "zzzzzzzz");
+	newLink = newLink.replace(/\s/g, "+");
+	$("#tweet-quote").attr("href", newLink);
+	console.log("newLink for twitter=", newLink);
+
+
+}
+
+
+
 
 
 //$("blockquote").css("backgroundColor", "yellow");
